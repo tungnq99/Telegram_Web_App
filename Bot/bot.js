7846a3aaaf29e78e96749d046bfff3b66a85ec07
tungnq99 <tungnq99@gmail.com>
2024-03-25 15:48:40 +0700

    update

--- Bot/bot.js
+++ Bot/bot.js
@@ -2,7 +2,7 @@ const { Telegraf } = require("telegraf");
 const TOKEN = "5153026043:AAEOA6Jgze21I8PGuZWKJoNMGY1Wzl6S5OI";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://celebrated-torte-184681.netlify.app/";
+const web_link = "https://telegram-web-app-ebon.vercel.app/";
 
 bot.start((ctx) =>
   ctx.reply("Hello, I am a bot", {
