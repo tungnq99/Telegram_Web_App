eb9318fb8c93b3913cd62be8a394f54d6db408e5
tungnq99 <tungnq99@gmail.com>
2024-03-25 16:58:19 +0700

    update

--- Bot/bot.js
+++ Bot/bot.js
@@ -2,7 +2,7 @@ const { Telegraf } = require("telegraf");
 const TOKEN = "7128159534:AAG2-VnA91iioIhjKqZq38w3b4e9TxX5EUY";
 const bot = new Telegraf(TOKEN);
 
-const web_link = "https://telegram-web-app-ebon.vercel.app/";
+const web_link = "https://telegram-web-app-liard.vercel.app/";
 
 bot.start((ctx) =>
   ctx.reply("Hello, I am a bot", {
